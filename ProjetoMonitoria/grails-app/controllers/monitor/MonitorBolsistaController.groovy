f5b7c85377867d8d8847aed2261f389271ab23b6
daninove <daninove@200268d6-2633-11df-98e6-c954eb596580>
2010-04-20 23:54:13 +0000

    Comentários para comentar

--- ProjetoMonitoria/grails-app/controllers/monitor/MonitorBolsistaController.groovy
+++ ProjetoMonitoria/grails-app/controllers/monitor/MonitorBolsistaController.groovy
@@ -4,6 +4,7 @@ package monitor
 /*
  *Temos que começar a fazer os java docs de cada classe, senão vai ficar muito dificil, fazer tudo depois,
  *vamos documentar.
+ *quem se habilita?
 */
 class MonitorBolsistaController {
 
--- ProjetoMonitoria/grails-app/controllers/professor/ProfessorController.groovy
+++ ProjetoMonitoria/grails-app/controllers/professor/ProfessorController.groovy
@@ -3,6 +3,7 @@ package professor
 /*
  *Temos que começar a fazer os java docs de cada classe, senão vai ficar muito dificil, fazer tudo depois,
  *vamos documentar.
+ **quem se habilita?
 */
 class ProfessorController {
     def scaffold = true
