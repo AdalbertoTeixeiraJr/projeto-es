65abb76c9b8ec300ee72e793bee02259e8ec6701
Adalbertojunior8 <Adalbertojunior8@200268d6-2633-11df-98e6-c954eb596580>
2010-09-16 16:28:19 +0000

    Adicionando analisador lexico e sintatico

---
+++ trunk/ProjetoMonitoria/lex-sint/tk.h
@@ -0,0 +1,110 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IF = 258,
+     ELSE = 259,
+     WHILE = 260,
+     DO = 261,
+     NOT = 262,
+     MINUS = 263,
+     PLUS = 264,
+     MULT = 265,
+     DIV = 266,
+     APAREN = 267,
+     FPAREN = 268,
+     BEG = 269,
+     END = 270,
+     ATRIB = 271,
+     TYPE_INT = 272,
+     MAIN = 273,
+     RELOP = 274,
+     LOGOP = 275,
+     INT = 276,
+     ID = 277
+   };
+#endif
+/* Tokens.  */
+#define IF 258
+#define ELSE 259
+#define WHILE 260
+#define DO 261
+#define NOT 262
+#define MINUS 263
+#define PLUS 264
+#define MULT 265
+#define DIV 266
+#define APAREN 267
+#define FPAREN 268
+#define BEG 269
+#define END 270
+#define ATRIB 271
+#define TYPE_INT 272
+#define MAIN 273
+#define RELOP 274
+#define LOGOP 275
+#define INT 276
+#define ID 277
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 20 "sintatico.y"
+
+ char* strval;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 102 "tk.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
---
+++ trunk/ProjetoMonitoria/lex-sint/tkvalues.h
@@ -0,0 +1,9 @@
+//VALORES DE RETORNO DOS TOKENS
+#define EQ 1
+#define NE 2
+#define GT 3
+#define LT 4
+#define GE 5
+#define LE 6
+#define AND 7
+#define OR 8
