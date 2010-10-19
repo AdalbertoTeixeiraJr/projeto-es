0f9930c664e39ec81d4488239773ffeb2f8e2cce
Adalbertojunior8 <Adalbertojunior8@200268d6-2633-11df-98e6-c954eb596580>
2010-10-19 01:38:40 +0000

    Arquivos de saida dos testes em ass add

--- trunk/ProjetoMonitoria/semantico/semantico.c
+++ trunk/ProjetoMonitoria/semantico/semantico.c
@@ -88,7 +88,7 @@ int main()
 				found2 = 1;
 				printf("LDI R1, %d\n", k);
 				printf("LD R2, R1\n");
-				printf("ST R0,R2\n");
+				printf("ST R0, R2\n");
 			}
 		}
 	   }
@@ -104,7 +104,9 @@ int main()
     
     //AND = and bit a bit
     else if((strcmp(valor4,"&") == 0) && (strcmp(valor2,"ATR") == 0)){
-	int found =0;
+	int found = 0;
+	int found2 = 0;
+	int found3 = 0;
         int j;
         for(j=0;j<index+1; j++){
             if(strcmp(lexico[j],valor1) == 0) {
@@ -112,15 +114,33 @@ int main()
                 printf("LDI R0 , %d\n",j);
 		if(atoi(valor3) >= -3 && atoi(valor3) <= 3){
 			printf("LDI R1, %s\n", valor3);	
+			found2 = 1;
 		}else{
 			printf("Variavel %s fora do intervalo\n",valor3 );
 			exit(1);
 		}
+		int k;
+		for(k=0; k<index+1;k++) {
+			if((strcmp(lexico[k], valor3) == 0) && (found2 == 0)) {
+				found2 = 1;
+				printf("LDI R1, %d\n", k);
+				printf("LD R1, R1\n");
+			}
+		}
 		if(atoi(valor5) >= -3 && atoi(valor5) <= 3){
 			printf("LDI R2, %s\n", valor5);	
+			found3 = 1;
 		}else{
 			printf("Variavel %s fora do intervalo\n",valor5 );
 			exit(1);
+		}
+		int k;
+		for(k=0; k<index+1;k++) {
+			if((strcmp(lexico[k], valor5) == 0) && (found3 == 0)) {
+				found2 = 1;
+				printf("LDI R2, %d\n", k);
+				printf("LD R2, R2\n");
+			}
 		}
 		printf("AND R3, R1, R2\n");
 		printf("ST R0, R3\n");
@@ -129,7 +149,15 @@ int main()
 	if(found == 0){
 		printf("Variavel %s nao esta declarada!\n", valor1);
 		exit(1);	
+	}
+	if(found2 == 0){
+		printf("Variavel %s nao esta declarada!\n", valor3);
+		exit(1);	
 	}	
+	if(found3 == 0){
+		printf("Variavel %s nao esta declarada!\n", valor5);
+		exit(1);	
+	}		
     }
     //Operacao Invalida
     else{
