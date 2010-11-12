8119b315855020ee9e172de1b6642a648c38569a
Adalbertojunior8 <Adalbertojunior8@200268d6-2633-11df-98e6-c954eb596580>
2010-11-12 20:52:23 +0000

    Script de teste imprimindo numero do teste

--- ProjetoMonitoria/runCompiler.sh
+++ ProjetoMonitoria/runCompiler.sh
@@ -13,16 +13,14 @@ rm -f ${tmp}/*.fail
 # analise files
 for i in $@; do
 	# prepara ambiente removendo arquivos antigos
-	out=$(basename $i)
-	rm -f ${out}
+	#out=$(basename $i)
+	#rm -f ${out}
 	# realiza a analise
-	echo "Iniciando analise lexica/sintatica do arquivo $i"
+	# echo "Iniciando analise lexica/sintatica do arquivo $i"
 	cat $i | lex-sint/compiler 1> sint_out.c
 	# verifica ocorrencia de falha lexica/sintatica
-	if [ $? -eq 0 ]; then
-		echo "Pode iniciar analise semantica e geracao de codigo do arquivo $i"
-	else
-		echo "Não Concluido possui erro lexico ou sintatico"
+	if [ $? -eq 1 ]; then
+		echo "Nao Concluido possui erro lexico ou sintatico"
 		mv sint_out.c sint_out.fail
 	fi
 done
